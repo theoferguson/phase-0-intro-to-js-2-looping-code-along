4e7345ce954bdc39acd502babf0cea2dc6726557
Theo Ferguson <ferguson.theo@gmail.com>
2021-07-23 14:36:54 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function writeCards(gifts, event) {
+    const messages = [];
+    for (let c = 0; c < gifts.length; c++) {
+        messages.push(`Thank you for the ${gifts[c]}, a wonderful ${event} gift!`);
+        debugger;
+    }
+    return messages;
+}
+
+writeCards(gifts, "birthday");
+
+
+function countDown(posInt) {
+    while (posInt >= 0) {
+        console.log(posInt--);
+    }
+}
+
+countDown(10);
